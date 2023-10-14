c576fcccadb963db56b96bacabd1c1ac7abfb1ab
Ac_K <Acoustik666@gmail.com>
2023-10-14 04:35:24 +0200

    Remove HLE arp services

--- src/Ryujinx/Ui/MainWindow.cs
+++ src/Ryujinx/Ui/MainWindow.cs
@@ -166,7 +166,6 @@ namespace Ryujinx.Ui
             _libHacHorizonManager = new LibHacHorizonManager();
 
             _libHacHorizonManager.InitializeFsServer(_virtualFileSystem);
-            _libHacHorizonManager.InitializeArpServer();
             _libHacHorizonManager.InitializeBcatServer();
             _libHacHorizonManager.InitializeSystemClients();
 
